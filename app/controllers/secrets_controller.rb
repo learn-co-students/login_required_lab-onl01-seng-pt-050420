e839203b2c9c7f09b08b9b1e9974b77d9a883cfb
C0mbed <matt@farncombe.co>
2020-09-23 05:25:14 -0600

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,10 @@
+class SecretsController < ApplicationController
+
+    def show
+        if session.include? :name
+          render '/sessions/new'
+        else
+          redirect_to '/login'
+        end
+    end
+end
