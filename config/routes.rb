db1a56134a18297f7c4a273f3a05ba2e501e5a1b
tpham13 <tpham0123@gmail.com>
2020-10-24 21:30:25 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
+  get '/show' => 'secrets#show'
+  post '/logout' => 'sessions#destroy'
+  root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
