21ceef380569f7258d32882100002bd64c005c5b
Zachary Lynch <zachary.lynch1337@gmail.com>
2021-02-16 08:42:01 +0000

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,12 @@
+class SecretsControlelr < ApplicationController 
+  before_action :require_login
+  skip_before_action :require_login, only: [:index]
+  
+  private 
+  
+  
+   def require_login
+    return head(:forbidden) unless session.include? :user_id
+  end
+  
+end
